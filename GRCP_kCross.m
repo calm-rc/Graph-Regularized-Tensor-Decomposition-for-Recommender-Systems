57a264f5e9a2b885bb06dd687d809c34bf5a06cd
calm-rc <83373984+calm-rc@users.noreply.github.com>
2022-10-28 22:07:46 +0200

    Update GRCP_kCross.m
    Updated variable names

--- GRCP_kCross.m
+++ GRCP_kCross.m
@@ -29,7 +29,7 @@ k_fold = int32(n/((1-opts.Tr_Split)*n));
 %% Graph Regularized Canonical Polyadiac (GRCP) Tensor Decomposition 
 for i = 1:k_fold
     kData = [Training_Set{i,1} Training_Set{i,2} Training_Set{i,3} Training_Set{i,4};Test_Set{i,1} Test_Set{i,2} Test_Set{i,3} Test_Set{i,4}];
-    [kA(i),klambda(i),kf(i),kTrain_RMSE(i),kTest_RMSE(i),kR_Time(i)] = ML_ALS(kData,opts);
+    [kA(i),klambda(i),kf(i),kTrain_NMSE(i),kTest_NMSE(i),kR_Time(i)] = ML_ALS(kData,opts);
 end
 
 %% Storing obtained results.
